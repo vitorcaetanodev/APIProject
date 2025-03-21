b4041e225975f5bbac92fa5d41a13e023a644922
Vitor Caetano <vitor.caetano.silva@usp.br>
2025-03-21 15:50:43 -0300

    Commit

--- ApiProject/obj/Debug/net6.0/ApiProject.AssemblyInfo.cs
+++ ApiProject/obj/Debug/net6.0/ApiProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+075d64773fcc9827a46fddd440e865290a2f18cc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18f1fc0f06c74fa7ba8b4b9d8d352143f109b45f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
