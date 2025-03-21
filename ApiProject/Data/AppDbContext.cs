7ab6a2ac1c2fe0478aa788f7fd050d3c7f32fed1
Vitor Caetano <vitor.caetano.silva@usp.br>
2025-03-21 13:59:09 -0300

    First installations
    First installations of the API Project

---
+++ ApiProject/Data/AppDbContext.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace ApiProject.Data
+{
+    public class AppDbContext : DbContext
+    {
+    }
+}
---
+++ ApiProject/Models/Model.cs
@@ -0,0 +1,9 @@
+﻿namespace ApiProject.Models
+{
+    public class Model
+    {
+        public int Id { get; set; }
+        public String Title { get; set; }
+        public bool Done { get; set; }
+    }
+}
